9c6735b82dedc1769b6d3e212e46e61f7cb81d8e
cameron-adams91 <cameron.adams@duke.edu>
2019-09-30 12:11:01 -0400

    Adds information

---
+++ ARGOSTracker.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTracker.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Cam Adams
+# Created on: Fall 2019
+#--------------------------------------------------------------
